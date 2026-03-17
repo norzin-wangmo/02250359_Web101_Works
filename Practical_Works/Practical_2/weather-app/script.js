e1faff24bda988026a8580e5c6df6a445baa17d0
norzin-wangmo <norzin.wangmo@icloud.com>
2026-03-17 14:29:59 +0600

    weather api

---
+++ Practical_Works/Practical_2/weather-app/script.js
@@ -0,0 +1,24 @@
+const API_KEY = "fe33e21951f4a9a3b46d736232a63946";
+
+function getWeather() {
+  const city = document.getElementById("city").value.trim();
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      if (data.cod === 200) {
+        document.getElementById("result").innerText =
+          "🌡 Temp: " + data.main.temp + "°C | " + data.weather[0].description;
+      } else {
+        document.getElementById("result").innerText =
+          "❌ " + data.message;
+      }
+    })
+    .catch(error => {
+      document.getElementById("result").innerText =
+        "Error fetching data";
+      console.log(error);
+    });
+}
